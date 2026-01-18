42576b8ce59e11b9c69d69987d5dc33aa755ecf6
Gao Ze <gaoze@u.nus.edu>
2026-01-18 16:22:59 +0800

    Level-1. Echo and modifications to readme Added an echo function to the chatbot to respond to input. Modified readme file

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Yoyo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("-----------------------------");
         System.out.println("Hello! I'm Yoyo.");
         System.out.println("What can I do for you?");
         System.out.println("-----------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("-----------------------------");
+            System.out.println(input);
+            System.out.println("-----------------------------");
+            input = sc.nextLine();
+        }
+
+        System.out.println("-----------------------------");
+        System.out.println("Bye. Hope to see you again!");
         System.out.println("-----------------------------");
     }
 }
